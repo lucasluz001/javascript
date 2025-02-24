7bf4f3063004782e1e9d0c9103eee0ddca4b6b93
Lucas Luz <lucas_lcs360@hotmail.com.br>
2025-02-24 13:51:47 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,18 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO(A)!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+console.log("")
+
+var gas = 50
+console.log(`Você esta enchendo o tanque com ${gas} Litros`)
+if (gas == 50){
+    console.log(`Tanque cheio`)
+} else {
+    console.log(`Ainda falta gasolina para encher o tanque`)
+}
+
+console.log("")
---
+++ aula11/ex009.js
@@ -0,0 +1,15 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log(`você é Brasileiro!`)
+} else {
+    console.log(`você é Estrangeiro!`)
+}
+
+var estudos = 'Javascript'
+console.log (`Estudando ${estudos}`)
+if (estudos == 'Javascript'){
+    console.log(`Você está no caminho para se tornar um programador de Javascript`)
+} else {
+    console.log(`Você está estudando ${estudos}`)
+}
