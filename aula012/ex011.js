a0daa3828fb7ba65d150d8a2522364b54cd44642
Lucas Luz <lucas_lcs360@hotmail.com.br>
2025-02-24 16:59:40 -0300

    Condição aninhada

--- aula012/ex011.js
+++ aula012/ex011.js
@@ -1,8 +1,18 @@
-var idade = 70
+var idade = 16
 if (idade >= 18 && idade <= 65){
     console.log(`Voto obrigatório`)
 } else if (idade >= 16){
     console.log(`Voto opcional`)
 } else {
-    console.log('Não vota')
+    console.log(`Não vota`)
+}
+
+console.log(``)
+
+if(idade >= 18 && idade < 65){
+    console.log(`Pode beber`)
+} else if (idade >= 65){
+    console.log(`Beber nessa idade é arriscado`)
+} else {
+    console.log(`Não pode beber`)
 }
