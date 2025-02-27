54df3029b0e3fe4f73bcab21bb92ae3e82556fe5
Lucas Luz <lucas_lcs360@hotmail.com.br>
2025-02-27 11:20:23 -0300

    Update script.js

--- aula012ex/ex015/script.js
+++ aula012ex/ex015/script.js
@@ -1,8 +1,8 @@
 enviar.addEventListener('click', verificar)
 
 function verificar(){
+    // DECLARAÇÃO DE VARIÁVEIS
     var formAno = window.document.getElementById('ano')
-    var formSexo = window.document.getElementById('sexo')
     var res = window.document.getElementById('res')
     var data = new Date()
     var ano = data.getFullYear()
@@ -11,12 +11,15 @@ function verificar(){
     if (formAno.value.length == 0 || formAno.value > ano){
         window.alert('[ERRO]Verifique os dados e tente novamente.')
     }else {
+        // SE NÃO ESTIVER VAZIO E MAIOR QUE O ANO ATUAL ENTÃO CRIE ESSAS VARIÁVEIS
         var formSexo = document.getElementsByName('sexo')
         var idade = ano - Number(formAno.value)
         var genero = ''
+        // CRIA UMA VARIÁVEL QUE CRIA UMA IMAGEM NO DOCUMENTO
         var img = document.createElement('img')
+        // PARA A IMAGEM CRIA O id = foto
         img.setAttribute('id', 'foto')
-        // VERIFICA O SEXO
+        // VERIFICA SE O SEXO MASCULINO TA SELECIONADO
         if(formSexo[0].checked){
             genero = 'Homem'
             if(idade >= 0 && idade < 10){
@@ -32,6 +35,7 @@ function verificar(){
                 //IDOSO
                 img.setAttribute('src', 'imagens/idoso.jpg')
             }
+            //VERIFICA SE O SEXO FEMININO TA SELECIONADO
         }else if(formSexo[1].checked){
             genero = 'Mulher'
             if(idade >= 0 && idade < 10){
@@ -48,8 +52,12 @@ function verificar(){
                 img.setAttribute('src', 'imagens/idosa.jpg')
             }
         }
+
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        // O MÉTODO appendChild DEVOLVE UMA REFERÊNCIA AO NÓ ADICIONADO
+        // res = PAI, ONDE SERÁ ADICIONADO A IMAGEM
+        // img = FILHO, A IMAGEM QUE SERÁ ADICIONADA A res
         res.appendChild(img)
     }
 }
