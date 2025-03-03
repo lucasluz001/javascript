12a2dc91a4c95a790a350cf9475fd5696758207c
Lucas Luz <lucas_lcs360@hotmail.com.br>
2025-03-03 19:53:15 -0300

    Exercício 1
    Programa que exibe 3 números na tela

---
+++ meus-exercicios/exe001/script.js
@@ -0,0 +1,10 @@
+// FAÇA UM PROGRAMA QUE EXIBA 3 NÚMERO NA TELA
+
+var res = window.document.querySelector('div#res')
+var n1 = 1029
+var n2 = 233
+var n3 = 123
+
+res.innerHTML = `<p>${n1}</p><br>`
+res.innerHTML += `<p>${n2}</p><br>`
+res.innerHTML += `<p>${n3}</p>`
