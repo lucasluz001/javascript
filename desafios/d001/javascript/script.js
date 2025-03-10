d2f157d46dd8712a3de847220871a03963b77039
Lucas Luz <lucas_lcs360@hotmail.com.br>
2025-03-10 15:41:04 -0300

    desafio 1

--- desafios/d001/javascript/script.js
+++ desafios/d001/javascript/script.js
@@ -3,14 +3,15 @@ var botao1 = window.document.querySelector('input#btn1')
 var botao2 = window.document.querySelector('input#btn2')
 var botao3 = window.document.querySelector('input#btn3')
 
+// Ao clicar no botão é disparado a função que possui o alert
 botao1.addEventListener('click', function(){
     window.alert('Você cliclou no 1º botão!')
 })
-
+// Ao clicar no botão é disparado a função que possui o alert
 botao2.addEventListener('click', function(){
     window.alert('Vcoê clicou no 2º botão!')
 })
-
+// Ao clicar no botão é disparado a função que possui o alert
 botao3.addEventListener('click', function(){
     window.alert('Você cliclou no 3º botão!')
 })
