181a035197905c1fb5b56613abf1139a9e7d540a
Lucas Luz <lucas_lcs360@hotmail.com.br>
2025-03-11 11:12:31 -0300

    desafio 7

---
+++ desafios/d007/script/script.js
@@ -0,0 +1,11 @@
+var botao = window.document.querySelector('input#btn')
+var res = window.document.querySelector('div#resultado')
+botao.addEventListener('click', converter)
+var cotacaoDolar = window.prompt('Antes de mais nada. Quanto está a cotação do dolar agora?')
+
+function converter(){
+    const real = window.prompt('Quantos R$ você tem na carteira?')
+    const conversao = real / cotacaoDolar
+    res.innerHTML = `<p>Você obteve US$${conversao.toLocaleString()}.</p>`
+
+}
