cf97911ff1ea59e254680dfdc2489840a6db2510
Lucas Luz <lucas_lcs360@hotmail.com.br>
2025-03-13 17:33:36 -0300

    Exercício 9

--- meus-exercicios/exe009/script.js
+++ meus-exercicios/exe009/script.js
@@ -3,9 +3,9 @@
 var res = window.document.querySelector('div#res')
 
 function encontraMaiorNumero(){
-    num1 = Number(prompt('Digite um número: '))
-    num2 = Number(prompt('Digite um número: '))
-    num3 = Number(prompt('Digite um número: '))
+    num1 = Number(prompt('Digite o primeiro número: '))
+    num2 = Number(prompt('Digite o segundo número: '))
+    num3 = Number(prompt('Digite o terceiro número: '))
     if (num1 > num2 && num1 > num3){
         res.innerHTML = `O maior número é ${num1}`
    }else if (num2 > num1 && num2 > num3){
